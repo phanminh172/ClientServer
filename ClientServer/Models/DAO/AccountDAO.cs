b556051d0193a277c651cc13c7aceb65bd722a6e
Minh Phan Ngoc <phanminh172@gmail.com>
2021-10-31 12:53:56 +0700

    Add project files.

---
+++ ClientServer/Models/DAO/AccountDAO.cs
@@ -0,0 +1,28 @@
+﻿using ClientServer.Models.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace ClientServer.Models.DAO
+{
+    public class AccountDAO
+    {
+        private ClientServerDbContext context = null;
+        public AccountDAO()
+        {
+            context = new ClientServerDbContext();
+        }
+        public bool Login(string userName, string password)
+        {
+            object[] sqlParams =
+            {
+                new SqlParameter("@UserName",userName),
+                new SqlParameter("@Password",password),
+            };
+            var res = context.Database.SqlQuery<bool>("Sp_Account_Login @UserName ,@Password", sqlParams).SingleOrDefault();
+            return res;
+        }
+    }
+}
