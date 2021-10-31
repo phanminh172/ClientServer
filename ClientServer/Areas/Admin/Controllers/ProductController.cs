0acca20eb3e86646ba3ddc71a5440609c7079f6d
Minh Phan Ngoc <phanminh172@gmail.com>
2021-10-31 17:13:23 +0700

    list product

--- ClientServer/Areas/Admin/Controllers/ProductController.cs
+++ ClientServer/Areas/Admin/Controllers/ProductController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ClientServer.Models.DAO;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,7 +12,81 @@ namespace ClientServer.Areas.Admin.Controllers
         // GET: Admin/Product
         public ActionResult Index()
         {
+            var iplCate = new ProductDAO();
+            var model = iplCate.ListAll();
+            return View(model);
+        }
+
+        // GET: Admin/Product/Details/5
+        public ActionResult Details(int id)
+        {
             return View();
+        }
+
+        // GET: Admin/Product/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/Product/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Admin/Product/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Admin/Product/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Admin/Product/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Admin/Product/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
         }
     }
 }
---
+++ ClientServer/Models/DAO/ProductDAO.cs
@@ -0,0 +1,22 @@
+﻿using ClientServer.Models.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ClientServer.Models.DAO
+{
+    public class ProductDAO
+    {
+        private ClientServerDbContext context = null;
+        public ProductDAO()
+        {
+            context = new ClientServerDbContext();
+        }
+        public List<ThongTinSanPham> ListAll()
+        {
+            var list = context.Database.SqlQuery<ThongTinSanPham>("Sp_Product_ListAll").ToList();
+            return list;
+        }
+    }
+}
