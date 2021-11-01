1ecf2bb874e617252471ca682f874e5dfbbfbaac
Minh Phan Ngoc <phanminh172@gmail.com>
2021-11-01 13:55:55 +0700

    update UI

---
+++ ClientServer/Controllers/diaryDetailController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ClientServer.Controllers
+{
+    public class diaryDetailController : Controller
+    {
+        // GET: diaryDetail
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: diaryDetail/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: diaryDetail/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: diaryDetail/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: diaryDetail/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: diaryDetail/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: diaryDetail/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: diaryDetail/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
