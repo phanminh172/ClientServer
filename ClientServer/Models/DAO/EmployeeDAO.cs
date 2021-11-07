ba0937cd40014894fc9c87da249a7ee216093198
= <=>
2021-11-08 06:05:58 +0700

    them sua xoa Employee

--- ClientServer/Models/DAO/EmployeeDAO.cs
+++ ClientServer/Models/DAO/EmployeeDAO.cs
@@ -18,5 +18,53 @@ namespace ClientServer.Models.DAO
             var list = context.ThongTinCongNhans.ToList();
             return list;
         }
+
+        public int Insert(ThongTinCongNhan employee)
+        {
+            context.ThongTinCongNhans.Add(employee);
+            context.SaveChanges();
+            return employee.MaCongNhan;
+        }
+        public ThongTinCongNhan GetById(int id)
+        {
+            return context.ThongTinCongNhans.SingleOrDefault(x => x.MaCongNhan == id);
+        }
+        public bool Update(ThongTinCongNhan entity)
+        {
+            try
+            {
+                var employee = context.ThongTinCongNhans.Find(entity.MaCongNhan);
+                employee.Hoten = entity.Hoten;
+                employee.GioiTinh = entity.GioiTinh;
+                employee.NgaySinh = entity.NgaySinh;
+                employee.QueQuan = entity.QueQuan;
+                employee.LuongBaoHiem = entity.LuongBaoHiem;
+                employee.LuongHopDong = entity.LuongHopDong;
+                employee.PhongBan = entity.PhongBan;
+                employee.ChucVu = entity.ChucVu;
+                context.SaveChanges();
+                //context.Entry(entity).State = System.Data.Entity.EntityState.Modified;
+                return true;
+            }
+            catch(Exception ex)
+            {
+                return false;
+            }
+        }
+        public bool Delete(int id)
+        {
+            try
+            {
+                var employee = context.ThongTinCongNhans.Find(id);
+                context.ThongTinCongNhans.Remove(employee);
+                context.SaveChanges();
+                return true;
+            }
+            catch(Exception ex)
+            {
+                return false;
+            }
+            
+        }
     }
 }
